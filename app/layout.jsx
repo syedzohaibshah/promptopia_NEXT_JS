f6c0c64c682c041575dde51d34c9afe046fa36da
Zohaib <zs3783999@gmail.com>
2024-07-03 10:41:31 +0500

    correct metadata issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,34 +2,27 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
+export const metadata = {
+  title: "Promptify",
+  description: "Discover and share AI prompts",
+};
 
-export const MetaData = {
- 
-title:"Promotopia",
-description:'discover and share AI prompts'
-
-}
-
-
-
- const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-    <body>
-    <Provider>
-        <div  className="main">
-            <div className="gradient"/>
-
-            </div>
-        <main className="app">
-        <Nav/>
-        {children}
-
-        </main>
-    </Provider>
-    </body>
-
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
-export default  RootLayout;
+  );
+};
+
+export default RootLayout;
